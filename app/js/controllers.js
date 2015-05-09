ead8c2295655b372b6e8b454f76ec85b75d516d9
math29 <maaath29@gmail.com>
2015-05-09 06:29:55 -0700

    Step 7 Completed

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,44 +1,10 @@
 'use strict';
 
 /* Controllers */
-var app = angular.module('AngularBeer', []);
+var app = angular.module('BeerControllers', []);
 
 app.controller('BeerListCtrl', ['$scope', '$http', function($scope, $http){
-	// $scope.beers = [
-	//   {
-	//     "alcohol": 6.8,
-	//     "name": "Affligem Blond",
-	//     "description": "Affligem Blonde, the classic clear blonde abbey ale, with a gentle roundness and 6.8% alcohol. Low on bitterness, it is eminently drinkable.",
-	//   	"age": 15
-	//   },
-	//   {
-	//     "alcohol": 8.5,
-	//     "name": "Affligem Tripel",
-	//     "description": "The king of the abbey beers. It is amber-gold and pours with a deep head and original aroma, delivering a complex, full bodied flavour. Pure enjoyment! Secondary fermentation in the bottle.",
-	//     "age" : 3
-	//   },
-	//   {
-	//     "alcohol": 9.2,
-	//     "name": "Rochefort 8",
-	//     "description": "A dry but rich flavoured beer with complex fruity and spicy flavours.",
-	//     "age" : 26
-	//   },
-	//   {
-	//     "alcohol": 11.3,
-	//     "name": "Rochefort 10",
-	//     "description": "The top product from the Rochefort Trappist brewery. Dark colour, full and very impressive taste. Strong plum, raisin, and black currant palate, with ascending notes of vinousness and other complexities.",
-	//     "age" : 3
-	//   },
-	//   {
-	//     "alcohol": 7,
-	//     "name": "Chimay Rouge",
-	//     "description": "This Trappist beer possesses a beautiful coppery colour that makes it particularly attractive. Topped with a creamy head, it gives off a slight fruity apricot smell from the fermentation. The aroma felt in the mouth is a balance confirming the fruit nuances revealed to the sense of smell. This traditional Belgian beer is best savoured at cellar temperature ",
-	//     "age": 12
-	//   }
-	// ];
-
-	$http.get('beers/beers.json').
-		success(function(data, status, headers, config) {
+	$http.get('beers/beers.json').success(function(data, status, headers, config) {
 			$scope.beers = data;
 		}).
 		error(function(data, status, headers, config) {
@@ -47,3 +13,7 @@ app.controller('BeerListCtrl', ['$scope', '$http', function($scope, $http){
 		});
 	$scope.order = "name";
 }]);
+
+app.controller('BeerDetailCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
+	$scope.beerId = $routeParams.beerId;
+}])
